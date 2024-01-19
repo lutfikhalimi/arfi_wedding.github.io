7283981d2178b81d022a6d231ac79f894fa626f3
Lutfi_Khalimi <73043607+lutfikhalimi@users.noreply.github.com>
2024-01-19 13:17:49 +0700

    Add files via upload

---
+++ laravel/tugas_css/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$p = 2;
+$l = 2;
+$a = 2;
+$t = 2;
+
+$lpp = $p * $l;
+echo 'Rumus Persegi Panjang';
+echo '<br>';
+echo "$p * $l = $lpp";
+
+$lp = $p * $l;
+echo 'Rumus Persegi';
+echo '<br>';
+echo "$p * $l = $lpp";
+
+?>

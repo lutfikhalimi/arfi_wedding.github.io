1fc6379c6c864d30cd6069b113e6ebbfde0a9685
lutfikhalimi <lutfikhalimi11@gmail.com>
2024-01-25 11:05:00 +0700

    Query Bulider

--- laravel/library_2/app/Http/Controllers/HomeController.php
+++ laravel/library_2/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Books;
 use App\Models\Member;
 use App\Models\Publisher;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -27,9 +28,33 @@ class HomeController extends Controller
     {
         //$books = Member::with('user')->get();
         //$books = Book::with('publisher')->get();
-        $publishers = Publisher::with('books')->get();
+        //$publishers = Publisher::with('books')->get();
 
-        return $publishers;
+        //no 1
+        $data = Member::select('*')
+                    ->join('users','users.member_id','=','members.id')
+                    ->get();
+
+        //no2
+        $data2 = Member::select('*')
+                    ->leftjoin('users','users.member_id','=','members.id')
+                    ->where('users.id', NULL)
+                    ->get();
+
+        //no3
+        $data3 = Transaction::select('members.id', 'members.name')
+                    ->rightjoin('members','members_id','=','transactions.members_id')
+                    ->where('transactions.member_id', NULL)
+                    ->get();
+
+        //no4
+        $data4 = Member::select('members.id', 'members.name', 'members.phone_number')
+                    ->join('transactions','transactions.members_id','=','members_id')
+                    ->orderBy('members_id', 'asc')
+                    ->get();
+
+
+        return $data4;
         return view('home');
     }
 }
